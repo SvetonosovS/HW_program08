9305661043c377745aa8fddfb2b14b88118398cc
SvetonosovS <tris23@bk.ru>
2022-10-02 23:31:17 +0300

    even_N

---
+++ Program_8.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+int i = 1;
+bool Not = true;
+Console.Write(userNumber + " -> ");
+
+while (i <= userNumber)
+{
+if (i % 2 != 1)
+    Console.Write(i + ", ");
+    Not = false;
+ i++;
+}
+if (Not)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
